90c166be264969b94dc7f15a299d92b45d0031cd
AungWinnHtut <aungwinnhtut@gmail.com>
2015-07-26 10:26:13 +0630

    modified array1.c to find tc and tf and save in file

--- Array/array1.c
+++ Array/array1.c
@@ -2,20 +2,20 @@
 #include<stdlib.h>
 int main()
 {
-	int i1=1;
-	int i2=2;
-	int i3=3;
-	int j[3];
+	int i;
+	float tf[100];
+	float tc[100];
+	FILE *fout;
+	fout =  fopen("temp.txt","w");
+	fprintf(fout," tf    - tc \n");
+	for(i=0;i<100;i++)
+	{
+		tf[i] = (i*100) + 1000; /*y=mx+c*/
+		tc[i] = (5 * (tf[i]-32)) /9;
+		fprintf(fout," %4.2f    = 		%4.2f\n",tf[i],tc[i]);
+	}
 
-	j[0]=1;
-	j[1]=4;
-	j[2]=110;
 
-	
-
-	printf("j[0]=%d size j[0]=%d bytes address j[0]=%0x\n",j[0],sizeof(j[0]),&j[0]);
-	printf("j[1]=%d size j[1]=%d bytes address j[1]=%0x\n",j[1],sizeof(j[1]),&j[1]);
-	printf("j[2]=%d size j[2]=%d bytes address j[2]=%0x\n",j[2],sizeof(j[2]),&j[2]);
-	printf("address of &j[0]=j %0x\n",j);
+	fclose(fout);
 	return 0;
 }
